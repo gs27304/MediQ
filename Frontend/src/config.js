95b957422b3b832caa5c4dd642c540c742c8b708
Manohar Singh <manoharsingh24689@gmail.com>
2026-04-05 10:18:07 +0530

    Update BASE_URL to include API version

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,4 +1,4 @@
 // export const BASE_URL="https://localhost:8000/api/v1"
-export const BASE_URL="https://mediq-backend-9pl5.onrender.com"
+export const BASE_URL="https://mediq-backend-9pl5.onrender.com/api/v1"
 
 export const getToken = () => localStorage.getItem('token');   
